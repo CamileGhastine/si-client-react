cbced909090f9da1631590aee86eaf4b68c83942
CamileGhastine <camileghastine@hotmail.com>
2022-04-06 15:19:11 +0200

    Implement html header
    Implementation of html header for walker space

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './css/App.css';
 import Walker_base from './component/Walker/Walker_base'
 
 function App() {
-  return (
-    <div>
-      <Walker_base />
-    </div>
-  );
+    return (
+        <div>
+            <Walker_base />
+        </div>
+    );
 }
 
 export default App;
--- src/component/Walker/_shared/Header.jsx
+++ src/component/Walker/_shared/Header.jsx
@@ -1,7 +1,15 @@
+import './css/headerStyle.css'
+
 export default function Header() {
     return (
-        <div>
-            header
-        </div>
+        <header class="row text-center pb-5">
+            <p className="simplexHeader col-12 d-flex align-items-end justify-content-center py-3">
+                <img src="/img/logoContoure.png" className="logoHeader" alt="logo Simplex-Immo" />
+                <span><span class="hidden">S</span>implex-Immo</span>
+
+            </p>
+            <p class="sentenceHeader col-12">Pourquoi payer ce que vous pouvez ne pas faire à la place d'un autre ?</p>
+            <div className="col-12"><a role="button" class="btn btn-warning">Voir une démo</a></div>
+        </header>
     );
 }
